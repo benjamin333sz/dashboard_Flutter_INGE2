567c88af8e0041633b91bef225034c96aaae6cfe
baphr1 <baptiste.behr@gmail.com>
2025-04-02 14:51:36 +0200

    boutons pour tri par régions v2

--- lib/widgets/region_button.dart
+++ lib/widgets/region_button.dart
@@ -1,11 +1,19 @@
-//créer un widget, avec bouttons, un qui permettrait d'afficher toutes les staions, et plusieurs
-//autres permettant de trier par régions
-//biens séparer le gtros boutton France, de la partie avec les bouttons région
-
-
 import 'package:flutter/material.dart';
 
-class FilterWidget extends StatelessWidget {
+class FranceButtonWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size(double.infinity, 40), // Hauteur réduite
+      ),
+      child: Text("France"),
+    );
+  }
+}
+
+class RegionsButtonWidget extends StatelessWidget {
   final List<String> regions = [
     "Île-de-France", "Auvergne-Rhône-Alpes", "Bretagne",
     "Normandie", "Occitanie", "Provence-Alpes-Côte d'Azur",
@@ -16,25 +24,45 @@ class FilterWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          "Tri par région",
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(height: 4), // Espacement réduit
+        ...regions.map((region) => Padding(
+          padding: const EdgeInsets.only(bottom: 4.0), // Moins d'espace entre les boutons
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              minimumSize: Size(double.infinity, 40), // Hauteur réduite
+            ),
+            child: Text(region),
+          ),
+        )).toList(),
+      ],
+    );
+  }
+}
+
+class FilterWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width * 0.3,
       color: Colors.white,
-      padding: EdgeInsets.all(10),
+      padding: EdgeInsets.all(8), // Moins de padding global
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          ElevatedButton(
-            onPressed: () {},
-            child: Text("France"),
-          ),
-          SizedBox(height: 10),
-          ...regions.map((region) => Padding(
-            padding: const EdgeInsets.only(bottom: 8.0),
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text(region),
-            ),
-          ))
+          FranceButtonWidget(),
+          SizedBox(height: 8),
+          Divider(),
+          SizedBox(height: 8),
+          RegionsButtonWidget(),
         ],
       ),
     );
